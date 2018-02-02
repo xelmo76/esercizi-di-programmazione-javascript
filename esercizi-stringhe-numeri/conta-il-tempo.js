395499fe03697e182df45c9bb474c603af77b086
xelmo76 <34725322+xelmo76@users.noreply.github.com>
2018-02-02 17:20:19 +0100

    Conta il tempo
    Che ne Pensi Albe? Può essere giusto così? ho fatto varie prove e torna quindi penso di si, ma un tuo feedback sarebbe super ;)

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,24 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+/*
+
+Problema: Trasformare un numero x di secondi in Ore, Minuti e secondi.
+
+Pseudo Codice
+
+- A = ho il numero totale di secondi
+- B = calcolo quanti minuti sono questi secondi (/60)
+- C = dal risultato calcolo quante ore sono questi minuti ( /60 )
+- D = calcolo il resto dei minuti con il modulo%
+- E = calcolo il resto dei secondi con il modulo%
+- F = stampo il risultato
+
+*/
+
+var time = 12560;
+var minutes = Math.round(time/60);
+var hours = Math.round(minutes/60);
+var minleft = minutes%60;
+var secleft = time%60;
+console.log (hours + ' ore', minleft + ' minuti', 'e '+ secleft + ' secondi');
