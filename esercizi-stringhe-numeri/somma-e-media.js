e5b23fa5001cd4343d8d766060ebbad02acc091c
xelmo76 <34725322+xelmo76@users.noreply.github.com>
2018-02-02 12:36:47 +0100

    Somma e Media

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,15 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+
+var first = 12;
+var second = 6;
+var third = 35;
+var fourth = 45;
+var fifth = 105;
+
+var sum = first + second + third + fourth + fifth;
+
+var average = sum/5;
+
+console.log (sum, average);
