cb29715731528203f0d85cee3aa29cfe7dce8400
xelmo76 <34725322+xelmo76@users.noreply.github.com>
2018-02-05 19:43:01 +0100

    Pari o Dispari

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -3,3 +3,23 @@
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
 */
+
+/*Problema:
+Ho un numero e devo far ritornare 0 se è pari o 1 se dispari
+
+posso usare il l'operatore % 2 (modulo) .
+In questo modo se il numero è pari mi restituirà 0 di resto mentre se è dispari
+mi restituira 1.
+
+
+
+*/
+
+
+
+var input = 213721983729734;
+
+if(input = input % 2)
+console.log(input +': il numero in input è dispari');
+else
+console.log(input +': il numero in input è pari');
