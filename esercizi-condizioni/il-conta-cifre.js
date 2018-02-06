e6a8673e06d044828de30b6b13fcaeaa33d02af0
xelmo76 <34725322+xelmo76@users.noreply.github.com>
2018-02-06 19:20:16 +0100

    Conta cifre

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -3,3 +3,8 @@
   Scrivi un programma che calcoli quante cifre sono contenute in un numero inserito in input (massimo 9999).
   http://www.imparareaprogrammare.it
 */
+
+
+var x = 9999;
+x = x.toString();
+console.log(x.length);
