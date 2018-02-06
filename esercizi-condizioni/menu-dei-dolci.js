035cfa362c5dbaa52a3c1bca60c79d35e1063497
xelmo76 <34725322+xelmo76@users.noreply.github.com>
2018-02-06 19:19:02 +0100

    Menu dei Dolci

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -10,3 +10,27 @@
   Se la scelta non è tra quelle elencate verrà stampata la scritta 'Dolce non disponibile' altrimenti verrà visualizzata la scelta effettuata 'Hai scelto il dolce X'.
   http://www.imparareaprogrammare.it
 */
+
+
+var menu = ('1. Tiramisù \n2. Torta della Nonna \n3. Cheesecake alla Nutella \n4. Macedonia');
+
+console.log(menu);
+
+var dolci = 1;
+
+switch (dolci){
+    case 1:
+    console.log('Complimenti hai scelto il Tiramisù!');
+    break;
+    case 2:
+    console.log('Complimenti hai scelto la Torta della Nonna!');
+    break;
+    case 3:
+    console.log('Complimenti hai scelto il Cheesecake alla Nutella!');
+    break;
+    case 4:
+    console.log('Complimenti hai scelto la Macedonia!');
+    break;
+    default:
+    console.log('Mi dispiace, ma il dolce scelto non risulta nel menù!')
+}
